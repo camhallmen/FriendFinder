55198d798e6d81f002585689ecc20fd49e206bef
thallmen <camhallmen@gmail.com>
2018-11-17 12:20:12 -0500

    initial test to deploy

---
+++ app/data/friends.js
@@ -0,0 +1,1 @@
+// Paceholder for Heroku deployment
---
+++ server.js
@@ -0,0 +1,29 @@
+// Jquery link
+<script
+  src="https://code.jquery.com/jquery-3.3.1.min.js"
+  integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+  crossorigin="anonymous">
+</script>
+
+// Dependencies
+// =============================================================
+var express = require("express");
+var path = require("path");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
